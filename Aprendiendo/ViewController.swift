29befe175aacb6781c5113031c9f74ad3cda661a
ahuamana <73962071@continental.edu.pe>
2023-12-19 23:10:16 -0500

    if applied this will implement libraries from cocoa pods to show dialog with blur

--- Aprendiendo/ViewController.swift
+++ Aprendiendo/ViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import PopupDialog
 
 class ViewController: UIViewController {
     //MARK: - Referencias UI
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var emailSwitch: UISwitch!
     
     @IBAction func loginButtonAction(_ sender: UIButton) {
       
@@ -24,24 +27,63 @@ class ViewController: UIViewController {
             print("empty passs")
             return
         }
-                
-        
-        
         
         if email == "carlos@gmail.com", password == "123" {
+            //handle saved email
+            if emailSwitch.isOn {
+                //Here we saved values in the user defaults
+                storage.set(email, forKey: emailKey)
+            } else {
+                storage.removeObject(forKey: emailKey)
+            }
             print("Bienvenido")
             performSegue(withIdentifier: "home_segue", sender: nil)
         } else {
-            print("Credenciles incorrectas")
+            showErrorDialog()
         }
         
     }
     
+    private let emailKey = "KEY_EMAIL"
+    private let storage = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view..
+        // De esta manera buscamos valores en los UserDefaults
+        
+        if let storageEmail = storage.string(forKey: emailKey) {
+            emailTextField.text = storageEmail
+            emailSwitch.isOn = true
+        } else {
+        
+            emailSwitch.isOn = false
+        }
+        
+        
+    }
     
+    private func showErrorDialog() {
+        // Prepare the popup assets
+        let title = "ERROR"
+        let message = "Credenciales invalidas"
+        let image = UIImage(named: "pexels-photo-103290")
+
+        // Create the dialog
+        let popup = PopupDialog(title: title, message: message, image: image)
+
+        // Create buttons
+        let buttonOne = CancelButton(title: "CANCEL") {
+            print("You canceled the car dialog.")
+        }
+
+        
+        // Add buttons to dialog
+        // Alternatively, you can use popup.addButton(buttonOne)
+        // to add a single button
+        popup.addButtons([buttonOne])
+
+        // Present dialog
+        self.present(popup, animated: true, completion: nil)
     }
 
 
